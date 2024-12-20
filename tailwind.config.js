4209e1e3818991b949bd793a333b03be148fcec7
Samantha Ogana <samanthaogana@gmail.com>
2024-12-21 01:14:21 +0300

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   media: false,
   theme: {
-    extend: {},
+    extend: {
     fontFamily: {
       greatVibes: ["Great Vibes"],
       poppins: ["Poppins"]
+    },
+    colors: {
+      yellowOrange: "#FFA500",
+      mintBlue: "#1b8a76"
+    }
     }
   },
   plugins: [],
