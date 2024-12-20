887db896b242b6615f7f22de8b90470d920500a0
Samantha Ogana <samanthaogana@gmail.com>
2024-12-21 01:16:22 +0300

    import LandingPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './index.css'
 import Navbar from './components/Navbar'
+import LandingPage from './pages/LandingPage'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <LandingPage />
     </>
   )
 }
