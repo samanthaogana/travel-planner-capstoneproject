a644e5e3a985f133d1d60e1c5c504d6e5e5ba4c6
Samantha Ogana <samanthaogana@gmail.com>
2024-12-23 02:36:49 +0300

    Add responsiveness for desktop view

--- src/pages/ExploreDestinations.jsx
+++ src/pages/ExploreDestinations.jsx
@@ -9,14 +9,16 @@ const ExploreDestinations = () => {
           Explore Destinations
         </h1>
         <form>
-          <div className="flex justify-center relative">
+          <div className="flex justify-center relative px-4 sm:px-11 ">
+            <div className="w-2/3 sm:w-[60%] relative">
             <input
-              className="rounded-md w-2/3 px-2 py-2 text-left border-2 pl-10 pr-4"
+              className="rounded-xl w-full sm:w-full px-24 py-2 text-left border-2 pl-10 pr-4"
               type="search"
               id="searchInput"
               placeholder="Search"
             ></input>
-           <button type="submit" className="absolute left-20 top-3 mx-2" ><FaSearch /></button>
+            </div>
+           <button type="submit" className="absolute left-20 top-3 mx-2 sm:left-72 sm:top-3" ><FaSearch /></button>
           </div>
         </form>
       </section>
