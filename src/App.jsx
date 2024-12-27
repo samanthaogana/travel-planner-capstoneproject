f587d6138d84e96cf029969033836bcba54617ed
Samantha Ogana <samanthaogana@gmail.com>
2024-12-28 01:17:02 +0300

    Import footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './index.css'
 import Navbar from './components/Navbar'
 import LandingPage from './pages/LandingPage'
 import ExploreDestinations from './pages/ExploreDestinations'
-
+import Footer from './components/Footer'
 
 function App() {
   
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <LandingPage />
       <ExploreDestinations />
+      <Footer />
       
     </>
   )
