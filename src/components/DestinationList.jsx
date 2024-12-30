5ba562864f4acd808387051c90ac8c4ef9d4eef3
Samantha Ogana <samanthaogana@gmail.com>
2024-12-30 17:56:59 +0300

    Modify function from setDestinations to openDestinationDetails

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -1,29 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { getDestinations } from '../services/amadeusService';
 
-const DestinationList = () => {
-  const [destinations, setDestinations] = useState([]);
+const DestinationList = ({destinations, openDestinationDetails}) => {
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const cities = ['new york', 'london', 'paris']; // Example cities
-        const data = await getDestinations(cities);
-        console.log('Fetched destinations:', data);
-        setDestinations(data);
-      } catch (error) {
-        console.error('Error fetching destinations:', error);
-      }
-    };
-    fetchData();
-  }, []);
+  console.log(destinations)
 
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      {destinations.map((destination, index) => (
-        <div key={index} className="bg-white shadow-lg rounded-lg p-4 w-64 text-center">
+      {destinations && destinations.map((destination, index) => (
+        <div key={index} className="bg-white shadow-lg rounded-lg p-4 w-64 text-center" onClick={()=> openDestinationDetails(destination)}>
           <h2 className="text-xl font-bold">{destination.name}</h2>
-          <p className="text-gray-600">Country: {destination.country}</p>
+          <p className="text-gray-600">Country: {destination.address.countryName}</p>
         </div>
       ))}
     </div>
