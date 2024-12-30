e705cff433a6fe92ea446131a4bf00bda39829ba
Samantha Ogana <samanthaogana@gmail.com>
2024-12-30 17:53:13 +0300

    Write search function for destinations

--- src/pages/ExploreDestinations.jsx
+++ src/pages/ExploreDestinations.jsx
@@ -1,31 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import { FaSearch } from "react-icons/fa";
 import DestinationList from "../components/DestinationList";
 
+import { getDestinationByCity } from "../services/amadeusService";
+
 const ExploreDestinations = () => {
+  const [searchQuery, setSearchQuery] = useState("")
+  const [searchResult, setSearchResult] = useState([])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const data  = await getDestinationByCity(searchQuery);
+
+    setSearchResult(data)
+  }
+
+  const openDestinationDetails = (destination) => {
+    console.log("Opening details for:", destination);
+    };
+
+
+  // console.log('YIPEE WE HAVE A RESPONSE', searchResult)
+
   return (
-    <main>
+    <main> 
       <section>
         <h1 className="font-greatVibes font-medium text-center text-seaBlue text-4xl my-4">
           Explore Destinations
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="flex justify-center relative px-4 sm:px-11 ">
             <div className="w-2/3 sm:w-[60%] relative">
             <input
               className="rounded-xl w-full sm:w-full px-24 py-2 text-left border-2 pl-10 pr-4"
               type="search"
               id="searchInput"
-              placeholder="Search"
+              placeholder="Search for a city..."
+              onChange={(e)=>setSearchQuery(e.target.value)}
             ></input>
             </div>
-           <button type="submit" className="absolute left-20 top-3 mx-2 sm:left-72 sm:top-3" ><FaSearch /></button>
+           <button type="submit" className="absolute left-20 top-3 mx-2 sm:left-72 sm:top-3" >
+            <FaSearch /></button>
           </div>
         </form>
       </section>
       
       <section>
-        <DestinationList />
+        <DestinationList destinations={searchResult} />
       </section>
     </main>
   );
