67a01564867fa0fc4b3fb02713eb79e04a7f1f57
Samantha Ogana <samanthaogana@gmail.com>
2025-01-09 01:44:40 +0300

    Modify footer and h1 tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ import { IoMdHeart } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <footer className="bg-mintBlue py-2">
+    <footer className="bg-mintBlue py-2 w-full">
   <div className="flex flex-col md:flex-row items-center justify-between">
     {/* <!-- Left side (H1 Text) --> */}
-    <h1 className="font-greatVibes text-xl md:text-2xl font-medium px-3 md:px-4 mb-2 md:mb-0">Travelscapes</h1>
+    <h1 className="font-greatVibes text-xl md:text-2xl font-black px-3 md:px-4 mb-2 md:mb-0">Travelscapes</h1>
 
     {/* <!-- Right side (Social media icons) --> */}
     <div className="flex gap-4 px-3 md:px-4 items-center mb-2 md:mb-0">
