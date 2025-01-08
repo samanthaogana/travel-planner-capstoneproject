ad41106f30476fdceff365a942a8f570492726e1
Samantha Ogana <samanthaogana@gmail.com>
2025-01-09 01:45:00 +0300

    import link from react-router-dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoIosClose } from "react-icons/io";
@@ -13,16 +14,16 @@ const Navbar = () => {
   return (
     <nav className="fixed bg-white top-0 left-0 w-full z-20">
       <div className="flex justify-between py-3 px-4 w-full">
-        <div className="font-greatVibes text-4xl text-black font-medium">
-          <h1>Travelscapes</h1>          
+        <div className="font-greatVibes text-4xl text-black font-black">
+          <h1><Link to="/">Travelscapes</Link></h1>          
         </div>
 
           {/* Desktop view */}
           <ul className="hidden sm:flex items-center mx-4 gap-7 font-medium font-poppins">
-          <li>Home</li>
-          <li>Destinations</li>
-          <li>Itinerary</li>
-          <li>Contact Us</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/destinations">Destinations</Link></li>
+          <li><Link to="/itinerary">Itinerary</Link></li>
+          <li><Link to="/contact-us">Contact Us</Link></li>
           <li><button className="font-semibold">Log in</button></li>
           <li><button className="font-bold border rounded-md px-2 py-2 bg-yellowOrange text-white">Sign up</button></li>
         </ul>
@@ -35,10 +36,11 @@ const Navbar = () => {
       {/* Mobile view */}
       {menu ? (
         <div className="sm:hidden bg-mintBlue  absolute top-16 right-0 w-full z-30 px-4 py-4">
-          <ul className="flex flex-col items-end gap-5 font-poppins text-white font-medium">
-            <li>Home</li>
-            <li>Itinerary</li>
-            <li>Contact Us</li>
+          <ul className="flex flex-col items-center gap-5 font-poppins text-white font-medium">
+            <li><Link to ="/">Home</Link></li>
+            <li><Link to= "/destinations">Destinations</Link></li>
+            <li><Link to="/itinerary">Itinerary</Link></li>
+            <li><Link to="/contact-us">Contact Us</Link></li>
             <li>
             <button className="font-semibold">Log in</button>
             </li>
